682e7f420b158c2ff6841799547f733a181973e4
RyDana <danriachi@hotmail.com>
2022-09-26 17:21:25 -0400

    color-changing nav bar, full width video on hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 window.addEventListener('load', (event) => {
     console.log("Page loaded.");
+
+    const nav = document.querySelector("#navbar");
+    const navLogoDark = nav.querySelector('#navLogoDark');
+    const navLogoLight = nav.querySelector('#navLogoLight');
+    console.log(nav);
+
+    window.onscroll = (event) => {
+        if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
+            nav.classList.add('bg-light');
+            nav.classList.remove('navbar-dark');
+            navLogoDark.style.opacity = 1;
+            navLogoLight.style.opacity = 0;
+        } else {
+            nav.classList.add('navbar-dark');
+            nav.classList.remove('bg-light');
+            nav.style.background = "none";
+            navLogoDark.style.opacity = 0;
+            navLogoLight.style.opacity = 1;
+        }
+    };
+
 });
